5902befe471e953254eff857309d8304507ada59
RaiZzens <118923268+RaiZzens@users.noreply.github.com>
2024-11-18 21:23:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,20 @@
 import os
 from telegram import Bot
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Application, CommandHandler
 
 # Bot-Token aus den Umgebungsvariablen holen
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 # Funktion für /start-Befehl
-def start(update, context):
-    update.message.reply_text('Hallo! Ich bin dein Telegram Bot.')
+async def start(update, context):
+    await update.message.reply_text('Hallo! Ich bin dein Telegram Bot.')
 
-# Updater und Dispatcher einrichten
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+# Application und Dispatcher einrichten
+application = Application.builder().token(TOKEN).build()
 
 # Befehl /start hinzufügen
 start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+application.add_handler(start_handler)
 
 # Bot starten
-updater.start_polling()
-updater.idle()
+application.run_polling()
